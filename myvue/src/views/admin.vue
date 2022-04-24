3b21154cb24cc6daf520a01115f23052e3661b64
gwx <121212@qq.com>
2022-04-25 07:40:23 +0800

    登录

---
+++ myvue/src/views/admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>欢迎来到后台管理功能</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style lang='scss' scoped >
+
+</style>
